5e2edacaff27d15dd95f8640dba15ea4e2335817
KEENTO <handball0321@gmail.com>
2022-01-12 22:49:33 -0800

    Fixed display of calculation

--- src/calculate.js
+++ src/calculate.js
@@ -1,7 +1,7 @@
 // import { checkMarks } from "./index.js";
 export const display = document.querySelector("input");
 
-const showResult = (val) => {
+export const showResult = (val) => {
   display.value = val;
 };
 
@@ -9,12 +9,12 @@ export const reset = () => {
   showResult("");
 };
 
-export const calculate = () => {
-  const value = display.value;
-  try {
-    const func = new Function("return " + value);
-    showResult(func().toString());
-  } catch (err) {
-    showResult(err);
-  }
-};
+// export const calculate = () => {
+//   const value = display.value;
+//   try {
+//     const func = new Function("return " + value);
+//     showResult(func().toString());
+//   } catch (err) {
+//     showResult(err);
+//   }
+// };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { calculate, reset, display } from "./calculate.js";
+import { display, showResult } from "./calculate.js";
 
 // Get DOM
 const calculateKeys = document.querySelectorAll(".calculateKey");
@@ -8,27 +8,47 @@ const resetKey = document.querySelector("#reset");
 
 // Define variable
 let cal;
+let markClicked = false;
 
 // Build out function
 function appendKey() {
   checkMarks();
-  console.log(this);
+  if (markClicked) display.value = "";
   cal === undefined ? (cal = this.value) : (cal += this.value);
-  // if (this.classList.contains("mark")) this.value = convertMark(this.id);
+  if (this.classList.contains("mark")) this.value = convertMark(this.id);
   display.value += this.value;
   console.log(cal);
+  markClicked = false;
+}
+
+function calculate() {
+  // const value = display.value;
+  const value = eval(cal);
+  try {
+    const func = new Function("return " + value);
+    showResult(func().toString());
+  } catch (err) {
+    showResult(err);
+  }
 }
 
 function convertMark(mark) {
-  if (mark === "multiply") {
-    return "✖️";
-  } else if (mark === "plus") {
-    return "➕";
-  } else if (mark === "minus") {
-    return "➖";
-  } else if (mark === "divide") {
-    return "➗";
-  }
+  // if (mark === "multiply") {
+  //   return "✖️";
+  // } else if (mark === "plus") {
+  //   return "➕";
+  // } else if (mark === "minus") {
+  //   return "➖";
+  // } else if (mark === "divide") {
+  //   return "➗";
+  // }
+  if (
+    mark === "multiply" ||
+    mark === "plus" ||
+    mark === "minus" ||
+    mark === "divide"
+  )
+    return "";
 }
 
 export const checkMarks = () => {
@@ -39,6 +59,14 @@ export const checkMarks = () => {
 
 function highlight() {
   this.classList.add("whiten");
+  markClicked = true;
+  // if (display.value !== "") display.value = "";
+}
+
+function reset() {
+  showResult("");
+  cal = undefined;
+  markClicked = false;
 }
 
 // Hook up the event
@@ -48,3 +76,8 @@ calculateKeys.forEach((calculateKey) =>
 marks.forEach((mark) => mark.addEventListener("click", highlight));
 equalKey.addEventListener("click", calculate);
 resetKey.addEventListener("click", reset);
+
+window.addEventListener("DOMContentLoaded", function () {
+  const call = "9*3-2+1000";
+  console.log(eval(call));
+});
